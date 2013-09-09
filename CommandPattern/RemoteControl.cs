705c8cefbc1e9ea10112a76e64aad263cea0d48c
jun <Johnjunzhang@gmail.com>
2013-09-09 15:30:48 +0800

    [Jun] Decouple instantiate command from execution request

--- CommandPattern/RemoteControl.cs
+++ CommandPattern/RemoteControl.cs
@@ -7,15 +7,29 @@ namespace CommandPattern
         private readonly Light light;
         private readonly Ceiling ceiling;
         private readonly Stereo stereo;
+        private readonly Dictionary<int, ICommand> getOffCommands;
+        private readonly Dictionary<int, ICommand> getOnCommands;
 
         public RemoteControl(Light light, Ceiling ceiling, Stereo stereo)
         {
             this.light = light;
             this.ceiling = ceiling;
             this.stereo = stereo;
+            getOffCommands = GetOffCommands();
+            getOnCommands = GetOnCommands();
         }
 
         public void On(int slot)
+        {
+            getOnCommands[slot].Execute();
+        }
+
+        public void Off(int slot)
+        {
+            getOffCommands[slot].Execute();
+        }
+
+        private Dictionary<int, ICommand> GetOnCommands()
         {
             var onCommands = new Dictionary<int, ICommand>();
             var lightOnCommand = new LightOnCommand(light);
@@ -24,11 +38,10 @@ namespace CommandPattern
             onCommands.Add(1, lightOnCommand);
             onCommands.Add(2, ceilingHighCommand);
             onCommands.Add(3, stereoOnCommand);
-
-            onCommands[slot].Execute();
+            return onCommands;
         }
 
-        public void Off(int slot)
+        private Dictionary<int, ICommand> GetOffCommands()
         {
             var offCommands = new Dictionary<int, ICommand>();
             var lightOffCommand = new LightOffCommand(light);
@@ -37,7 +50,7 @@ namespace CommandPattern
             offCommands.Add(1, lightOffCommand);
             offCommands.Add(2, ceilingOffCommand);
             offCommands.Add(3, stereoOffCommand);
-            offCommands[slot].Execute();
+            return offCommands;
         }
     }
 }
