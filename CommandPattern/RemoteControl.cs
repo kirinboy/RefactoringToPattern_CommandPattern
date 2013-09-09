b2da16c7fb51d25cc55a89ca0224e36df6ed2684
jun <Johnjunzhang@gmail.com>
2013-09-09 16:01:33 +0800

    [Jun] Parameterize clients with different request

--- CommandPattern/RemoteControl.cs
+++ CommandPattern/RemoteControl.cs
@@ -7,16 +7,6 @@ namespace CommandPattern
         private readonly Dictionary<int, ICommand> onCommands = new Dictionary<int, ICommand>();
         private readonly Dictionary<int, ICommand> offCommands = new Dictionary<int, ICommand>();
 
-        public RemoteControl(Light light, Ceiling ceiling, Stereo stereo)
-        {
-            SetOffCommand(1, new LightOffCommand(light));
-            SetOffCommand(2, new CeilingOffCommand(ceiling));
-            SetOffCommand(3, new StereoOffCommand(stereo));
-            SetOnCommand(1, new LightOnCommand(light));
-            SetOnCommand(2, new CeilingHighCommand(ceiling));
-            SetOnCommand(3, new StereoOnCommand(stereo));
-        }
-
         public void On(int slot)
         {
             onCommands[slot].Execute();
@@ -27,12 +17,12 @@ namespace CommandPattern
             offCommands[slot].Execute();
         }
 
-        private void SetOnCommand(int slot, ICommand command)
+        public void SetOnCommand(int slot, ICommand command)
         {
             onCommands.Add(slot, command);
         }
 
-        private void SetOffCommand(int slot, ICommand command)
+        public void SetOffCommand(int slot, ICommand command)
         {
             offCommands.Add(slot, command);
         }
