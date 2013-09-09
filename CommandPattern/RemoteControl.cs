1259700c65e16429803e8e62da2ed4067c9d48b1
jun <Johnjunzhang@gmail.com>
2013-09-09 15:42:45 +0800

    [Jun] Remove temporary field

--- CommandPattern/RemoteControl.cs
+++ CommandPattern/RemoteControl.cs
@@ -4,52 +4,32 @@ namespace CommandPattern
 {
     public class RemoteControl
     {
-        private readonly Light light;
-        private readonly Ceiling ceiling;
-        private readonly Stereo stereo;
-        private readonly Dictionary<int, ICommand> getOffCommands;
-        private readonly Dictionary<int, ICommand> getOnCommands;
         private readonly Dictionary<int, ICommand> onCommands = new Dictionary<int, ICommand>();
         private readonly Dictionary<int, ICommand> offCommands = new Dictionary<int, ICommand>();
 
         public RemoteControl(Light light, Ceiling ceiling, Stereo stereo)
         {
-            this.light = light;
-            this.ceiling = ceiling;
-            this.stereo = stereo;
-            getOffCommands = GetOffCommands();
-            getOnCommands = GetOnCommands();
+            offCommands.Add(1, new LightOffCommand(light));
+            offCommands.Add(2, new CeilingOffCommand(ceiling));
+            offCommands.Add(3, new StereoOffCommand(stereo));
+            SetOnCommand(1, new LightOnCommand(light));
+            SetOnCommand(2, new CeilingHighCommand(ceiling));
+            SetOnCommand(3, new StereoOnCommand(stereo));
         }
 
         public void On(int slot)
         {
-            getOnCommands[slot].Execute();
+            onCommands[slot].Execute();
         }
 
         public void Off(int slot)
         {
-            getOffCommands[slot].Execute();
-        }
-
-        private Dictionary<int, ICommand> GetOnCommands()
-        {
-            SetOnCommand(1, new LightOnCommand(light));
-            SetOnCommand(2, new CeilingHighCommand(ceiling));
-            SetOnCommand(3, new StereoOnCommand(stereo));
-            return onCommands;
+            offCommands[slot].Execute();
         }
 
         private void SetOnCommand(int slot, ICommand command)
         {
             onCommands.Add(slot, command);
-        }
-
-        private Dictionary<int, ICommand> GetOffCommands()
-        {
-            offCommands.Add(1, new LightOffCommand(light));
-            offCommands.Add(2, new CeilingOffCommand(ceiling));
-            offCommands.Add(3, new StereoOffCommand(stereo));
-            return offCommands;
         }
     }
 }
